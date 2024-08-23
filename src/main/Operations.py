8c2d787c235690e7678da0789eb3426a477ed45d
Luis Pedro Lira <luispelirak130@gmail.com>
2024-08-22 22:34:12 -0600

    Changes in Operations

--- src/main/Operations.py
+++ src/main/Operations.py
@@ -1,55 +1,53 @@
 import Set
 class Operations:
     
-    # 'union' retorna la unión de los n conjuntos.
+    def __init__(self, elements=None):
+        if elements is None:
+            elements = []
+        self.elements = elements
+    
+    # 'union' retorna la unión de los conjuntos.
     def union(self, other_set):
         result = Set()
         result.elements = self.elements.copy()
         for element in other_set.elements:
             if element not in result.elements:
-                result.add_elementent(element)
+                result.add_element(element)
         return result
 
-    # 'intersection' retorna la intersección de los n conjuntos.
+    # 'intersection' retorna la intersección de los conjuntos.
     def intersection(self, other_set):
         result = Set()
         for element in self.elements:
             if element in other_set.elements:
-                result.add_elementent(element)
+                result.add_element(element)
         return result
 
-    # 'difference' retorna la diferencia de los n conjuntos.
+    # 'difference' retorna la diferencia de los conjuntos.
     def difference(self, other_set):
         result = Set()
         for element in self.elements:
             if element not in other_set.elements:
-                result.add_elementent(element)
+                result.add_element(element)
         return result
 
-    '''
-        'symmetric_difference' retorna la diferencia simétrica de los n conjuntos.
-        Basicamente, la diferencia simétrica se puede expresar de dos maneras:
-        
-        1. (A - B) U (B - A)
-        2. (A U B) - (A ∩ B)
-        
-        En este caso, se implemento la primera opción.
-    '''
+    # 'symmetric_difference' retorna la diferencia simétrica de los conjuntos.
     def symmetric_difference(self, other_set):
         result = Set()
-        for elementent in self.elements:
-            if elementent not in other_set.elements:
-                result.add_elementent(elementent)
-        for elementent in other_set.elements:
-            if elementent not in self.elements:
-                result.add_elementent(elementent)
+        for element in self.elements:
+            if element not in other_set.elements:
+                result.add_element(element)
+        for element in other_set.elements:
+            if element not in self.elements:
+                result.add_element(element)
         return result
 
+    # 'complement' retorna el complemento del conjunto en relación con un conjunto universal.
     def complement(self):
         universal_set = Set()
         for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789':
             if char not in self.elements:
-                universal_set.add_elementent(char)
+                universal_set.add_element(char)
         return universal_set
 
     def __str__(self):
